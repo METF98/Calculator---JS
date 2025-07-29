975b4bdfe279b3adbbb11b0ae65655da2e8bdad7
Miguel Ticaray <142277133+METF98@users.noreply.github.com>
2025-07-29 09:33:18 -0400

    formateo

--- src/js/main.js
+++ src/js/main.js
@@ -16,27 +16,26 @@ let isResult = false;
 
 /**
  * Eventos de los botones
-*/
-btn_number.forEach(button => {
-  button.addEventListener("click",() => addDisplay(button.textContent));
+ */
+btn_number.forEach((button) => {
+  button.addEventListener("click", () => addDisplay(button.textContent));
 });
 
+btn_operator.forEach((button) => {
+  button.addEventListener("click", () => addDisplay(button.textContent, true));
+});
 
-btn_operator.forEach(button => {
-  button.addEventListener("click",() => addDisplay(button.textContent,true));
-})
-
-btn_point.addEventListener("click",() => addDisplay(btn_point.textContent));
+btn_point.addEventListener("click", () => addDisplay(btn_point.textContent));
 btn_igual.addEventListener("click", () => calculateResult());
 
 /**
  * Funcion que nos permite
  * limpiar un dato
-*/
-btn_delete.addEventListener("click",() => {
-  if(valor.textContent.length > 1){
-    valor.textContent = valor.textContent.toString().slice(0,-1);
-  }else{
+ */
+btn_delete.addEventListener("click", () => {
+  if (valor.textContent.length > 1) {
+    valor.textContent = valor.textContent.toString().slice(0, -1);
+  } else {
     valor.textContent = 0;
   }
 });
@@ -44,8 +43,8 @@ btn_delete.addEventListener("click",() => {
 /**
  * Funcion que nos permite
  * limpiar todos los datos
-*/
-btn_clear.addEventListener("click",() => {
+ */
+btn_clear.addEventListener("click", () => {
   valor.textContent = 0;
   historial.textContent = "";
   updateDisplay = false;
@@ -59,46 +58,46 @@ btn_clear.addEventListener("click",() => {
  * @param {Boolean} operador @example false or true
  * @returns {String} @example "1" or "1 + " or "1 + 1"
  */
-function addDisplay(dato,operador = false) {
-  if(!operador){
+function addDisplay(dato, operador = false) {
+  if (!operador) {
     if (valor.textContent == 0 || !updateDisplay) {
-      if(validaDisplay(valor.textContent)){
-        if(isResult) {
+      if (validaDisplay(valor.textContent)) {
+        if (isResult) {
           historial.textContent = "";
           valor.textContent = dato;
           updateDisplay = true;
           isResult = false;
-        }else{
+        } else {
           valor.textContent = dato;
           updateDisplay = true;
         }
-      }else{
+      } else {
         display.classList.add("bg-red-500");
         setTimeout(() => {
           display.classList.remove("bg-red-500");
-        },500);
+        }, 500);
       }
-    }else{
-      if(validaDisplay(valor.textContent)){
+    } else {
+      if (validaDisplay(valor.textContent)) {
         valor.textContent += dato;
-      }else{
+      } else {
         display.classList.add("bg-red-500");
         setTimeout(() => {
           display.classList.remove("bg-red-500");
-        },500);
+        }, 500);
       }
     }
-  }else if(operador){
-    if(valor.textContent != 0){
-      if(validaDisplay(valor.textContent)){
+  } else if (operador) {
+    if (valor.textContent != 0) {
+      if (validaDisplay(valor.textContent)) {
         historialDisplay(dato);
         updateDisplay = true;
         valor.textContent = 0;
-      }else{
+      } else {
         display.classList.add("bg-red-500");
         setTimeout(() => {
           display.classList.remove("bg-red-500");
-        },500);
+        }, 500);
       }
     }
   }
@@ -108,38 +107,37 @@ function addDisplay(dato,operador = false) {
  * Validamos el tamaño del display para que no se sobrepase
  * @returns {Boolean} @example true or false
  */
-function validaDisplay(){
+function validaDisplay() {
   if (valor.textContent.length <= 25) {
     if (valor.textContent.length <= 15) {
       valor.classList.remove("text-[1.5rem]");
       valor.classList.add("text-[2.5rem]");
       return true;
-    }else{
+    } else {
       valor.classList.remove("text-[2.5rem]");
       valor.classList.add("text-[1.5rem]");
       return true;
     }
-  }else{
+  } else {
     return false;
   }
 }
-
 
 /**
  * Funcion que nos permite guardar la operacion en el historial
  * @param {String} operator @example "1 +" or "1 + 1"
  */
 function historialDisplay(operator) {
-    if (isResult) {
-      validaDisplay();
-      historial.textContent = ' ' + valor.textContent + ' ' + operator + ' ';
-      valor.textContent = 0;
-      isResult = false;
-    }else{
-      validaDisplay();
-      historial.textContent += ' ' + valor.textContent + ' ' + operator + ' ';
-      valor.textContent = 0;
-    }
+  if (isResult) {
+    validaDisplay();
+    historial.textContent = " " + valor.textContent + " " + operator + " ";
+    valor.textContent = 0;
+    isResult = false;
+  } else {
+    validaDisplay();
+    historial.textContent += " " + valor.textContent + " " + operator + " ";
+    valor.textContent = 0;
+  }
 }
 
 /**
@@ -147,12 +145,12 @@ function historialDisplay(operator) {
  * @returns {String} @example "1"
  */
 function calculateResult() {
-  if (historial.textContent && valor.textContent &&  valor.textContent >= 0) {
-    const expression = historial.textContent + ' ' + valor.textContent;
+  if (historial.textContent && valor.textContent && valor.textContent >= 0) {
+    const expression = historial.textContent + " " + valor.textContent;
     try {
       const result = operar(expression);
-      historial.textContent = expression ;
-      if(result == "Math Error"){
+      historial.textContent = expression;
+      if (result == "Math Error") {
         valor.textContent = result;
         updateDisplay = false;
         isResult = false;
@@ -161,34 +159,39 @@ function calculateResult() {
           display.classList.remove("bg-red-500");
           historial.textContent = "";
           valor.textContent = 0;
-        },1000);
-      }else{
+        }, 1000);
+      } else {
         valor.textContent = result;
         validaDisplay();
         updateDisplay = false;
         isResult = true;
       }
-    }catch (e) {
-      if (valor.textContent != 0){
+    } catch (e) {
+      if (valor.textContent != 0) {
         let error = document.createElement("p");
         error.textContent = "Debes Seleccionar un Operador";
-        error.classList.add("text-red-500","text-center","font-bold","text-2xl","mb-6");
+        error.classList.add(
+          "text-red-500",
+          "text-center",
+          "font-bold",
+          "text-2xl",
+          "mb-6"
+        );
         body.insertBefore(error, body.children[1]);
         console.log(e);
         setTimeout(() => {
           error.remove();
-        },"2000");
-      }else{
+        }, "2000");
+      } else {
         historial.textContent = "";
         valor.textContent = 0;
       }
     }
-  }else{
+  } else {
     historial.textContent = "";
     valor.textContent = 0;
   }
 }
-
 
 /**
  * Funcion que nos permite operar los datos
@@ -197,10 +200,13 @@ function calculateResult() {
  * @returns {String} @example "2" or "Math Error"
  */
 function operar(evaluacion) {
-  if(eval(evaluacion) == "Infinity" || eval(evaluacion) == "NaN" || eval(evaluacion) == "-Infinity"){
+  if (
+    eval(evaluacion) == "Infinity" ||
+    eval(evaluacion) == "NaN" ||
+    eval(evaluacion) == "-Infinity"
+  ) {
     return "Math Error";
-  }else{
+  } else {
     return eval(evaluacion);
   }
-
 }
